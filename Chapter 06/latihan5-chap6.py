437d985e4f253621b5d07ce872289fb76f54e495
Envy Noxious <ahmadadli02@student.uns.ac.id>
2021-11-08 21:38:04 +0700

    Create latihan5-chap6.py

---
+++ Chapter 06/latihan5-chap6.py
@@ -0,0 +1,6 @@
+from statistik import *
+
+print('Data (5, 10, 4, 9, 30, 16, 2, 11) Jika dijumlahkan menjadi', sum(5, 10, 4, 9, 30, 16, 2, 11), '\n')
+print('Data (81, 98, 12, 83, 45, 77, 69, 30, 56) Rata-ratanya adalah', average(81, 98, 12, 83, 45, 77, 69, 30, 56), '\n')
+print('Data terbesar dari (5, 10, 4, 9, 30, 16, 2, 11) adalah', max(5, 10, 4, 9, 30, 16, 2, 11), '\n')
+print('Data terkecil dari (81, 98, 12, 83, 45, 77, 69, 30, 56) adalah', min(81, 98, 12, 83, 45, 77, 69, 30, 56))
