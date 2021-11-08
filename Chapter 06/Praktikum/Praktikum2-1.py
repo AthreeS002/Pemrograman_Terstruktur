aa5ed2450d8aa0edd3d700ca2d376caaafdefe3c
Envy Noxious <ahmadadli02@student.uns.ac.id>
2021-11-08 21:38:15 +0700

    Create Praktikum2-1.py

---
+++ Chapter 06/Praktikum/Praktikum2-1.py
@@ -0,0 +1,11 @@
+def luasSegitiga(a, t):
+    luas = a * t / 2
+    return luas
+
+alas = 15
+tinggi = 45
+print('Luas segitiga dengan alas', alas, 'dan tinggi', tinggi, 'adalah', luasSegitiga(alas, tinggi))
+
+
+#nilai alas yang di print, itu nanti dimasukkin ke def(luasSegitiga) yang pertama
+#sama halnya dengan yang nilai tinggi yg bawah/print, nanti masuk ke def(luasSegitiga) yang atas itu
