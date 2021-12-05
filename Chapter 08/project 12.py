9d813d7de858978b3ffda0891aaef86c9fd6170c
Envy Noxious <ahmadadli02@student.uns.ac.id>
2021-12-05 20:20:44 +0700

    Create project 12.py

---
+++ Chapter 08/project 12.py
@@ -0,0 +1,74 @@
+def tampilkan_data(data):
+    print('----------------------')
+    print('Daftar Buah: ')
+    for key, value in data.items():
+        print('-', key, ': ', value)
+
+
+def beli_buah():
+    total_semua = 0
+    while True:
+        try:
+            print('----------------------')
+            nama = input('Masukkan nama buah: ')
+            harga = buah[nama]
+            jumlah = int(input('Berapa Kilogram?  : '))
+            total_harga = harga * jumlah
+            total_semua += total_harga
+            tambah = input('\nBeli buah yang lain (Y/N)? ')
+            if tambah == 'n' or tambah == 'N':
+                print('----------------------')
+                print('Total harga  :', total_semua)
+                break
+        except KeyError:
+            print('\n[Data Error]')
+
+
+def tambah_data(data):
+    print('----------------------')
+    nama = input('Masukkan nama buah yang akan ditambahkan: ')
+    if nama not in data:
+        harga = int(input('Masukkan harga buah: '))
+        data[nama.lower()] = harga
+        print('[OK]')
+    else:
+        print('\n[Sudah ada]')
+
+
+def hapus_data(data):
+    print('----------------------')
+    nama = input('Masukkan nama buah yang ingin dihapus: ')
+    if nama.lower() in data:
+        del(data[nama.lower()])
+        print('OK')
+    else:
+        print('[Data Error]')
+
+
+def tampilkan_menu():
+    print('----------------------')
+    print('A. Tambah data buah\nB. Hapus data buah\nC. Beli buah\nD. Exit')
+    print('----------------------')
+    return input('Masukkan pilihan: ')
+
+
+buah = {'Apel': 5000,
+        'Jeruk': 8500,
+        'Mangga': 7800,
+        'Duku': 6500}
+
+while True:
+    pilihan = tampilkan_menu()
+    if pilihan == 'a' or pilihan == 'A':
+        tambah_data(buah)
+        tampilkan_data(buah)
+    elif pilihan == 'b' or pilihan == 'B':
+        tampilkan_data(buah)
+        hapus_data(buah)
+    elif pilihan == 'c' or pilihan == 'C':
+        tampilkan_data(buah)
+        beli_buah()
+    elif pilihan == 'd' or pilihan == 'D':
+        exit('\n[Exit ...]')
+    else:
+        print('[Input Error]')
